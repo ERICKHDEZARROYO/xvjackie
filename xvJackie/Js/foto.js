0486cbe50ea1adfbfe55301a86de1b2f01b1125c
ERICKHDEZARROYO <134813394+ERICKHDEZARROYO@users.noreply.github.com>
2023-10-23 00:47:49 -0600

    ok

--- xvJackie/Js/foto.js
+++ xvJackie/Js/foto.js
@@ -2,7 +2,7 @@ var counter = 1;
 setInterval(function(){
   document.getElementById('radio' + counter).checked = true;
   counter++;
-  if(counter > 13){
+  if(counter > 14){
     counter = 1;
   }
 }, 5000);
